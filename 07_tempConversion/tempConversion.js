4d291a150679436334189663b18091365946a7aa
Ahleever <monterooliver97@gmail.com>
2024-08-18 13:18:56 -0700

    Completed exercises 06 through to 09

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsiusConversion = Math.round(((temp - 32) * (5/9))*10)/10;
+  return celsiusConversion
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheitConversion = Math.round(((temp* (9/5)) +32)*10)/10;
+  return fahrenheitConversion;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (stringInput) {
+let string = stringInput.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+let left =0;
+let right = string.length -1;
+while (left < right){
+    if(string.charAt(left) !== string.charAt(right)){
+        return false;
+    }
+    left++;
+    right--;
+    }
+return true
 };
 
 // Do not edit below this line
