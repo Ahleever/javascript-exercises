6d9575a10886a497e4821b941b19c71a6fb313f3
Ahleever <monterooliver97@gmail.com>
2024-08-22 21:18:26 -0700

    Finished Fibonnacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    number = parseInt(number)
+    let start = 1;
+    let result = 1;
+    let total = 0;
+    while(number > 1){
+        for (let i = 1; i < number; i++){
+            total = start + result;
+            start = result;
+            result = total;
+        }
+        return start
+    }
+    if(number < 0){
+        return "OOPS"
+    }
+    return number
 };
 
 // Do not edit below this line
